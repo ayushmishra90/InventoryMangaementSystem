e222f61d7a2918e9d39a18402730dae72d14b44b
ayushmishra90 <145120302+ayushmishra90@users.noreply.github.com>
2026-07-04 14:05:16 +0530

    updated CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,7 +45,8 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://inventorymangaementsystem.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
